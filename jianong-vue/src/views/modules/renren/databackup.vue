c658d2e64e41ce5bb262800bf3d12cde51f8bf7b
coder-zrl <970586718@qq.com>
2022-04-13 02:04:29 +0800

    fix:data backup detail

--- jianong-vue/src/views/modules/renren/databackup.vue
+++ jianong-vue/src/views/modules/renren/databackup.vue
@@ -6,7 +6,7 @@
       </el-form-item>
       <el-form-item>
         <el-button @click="getDataList()">查询</el-button>
-        <el-button v-if="isAuth('renren:databackup:save')" type="primary" @click="addOrUpdateHandle()">新增</el-button>
+        <el-button v-if="isAuth('renren:databackup:save')" type="primary" @click="addOrUpdateHandle()">备份</el-button>
         <el-button v-if="isAuth('renren:databackup:delete')" type="danger" @click="deleteHandle()" :disabled="dataListSelections.length <= 0">批量删除</el-button>
       </el-form-item>
     </el-form>
